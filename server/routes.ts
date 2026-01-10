d4d3cfd300105a177ee9b040fe6a100121bccb18
potterfan92993 <109814790+potterfan92993@users.noreply.github.com>
2026-01-10 14:34:05 -0700

    Update routes.ts
    removed auto generating code that didnt work

--- server/routes.ts
+++ server/routes.ts
@@ -23,20 +23,6 @@ const verifyTwitchSignature = (req: Request) => {
   return `sha256=${hmac}` === signature;
 };
 
-async function getAppAccessToken() {
-  const response = await fetch('https://id.twitch.tv/oauth2/token', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-    body: new URLSearchParams({
-      client_id: process.env.TWITCH_CLIENT_ID,
-      client_secret: process.env.TWITCH_CLIENT_SECRET,
-      grant_type: 'client_credentials'
-    })
-  });
-  const data = await response.json();
-  return data.access_token;
-}
-
 export async function registerRoutes(
   httpServer: Server,
   app: Express
